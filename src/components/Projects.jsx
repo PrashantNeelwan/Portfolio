95724018cb910fb35db9e017eb768f8a1fb3d2cf
PrashantNeelwan <prashantneelwan@gmail.com>
2026-01-28 13:28:15 -0500

    upadated image path

--- src/components/Projects.jsx
+++ src/components/Projects.jsx
@@ -5,7 +5,7 @@ export default function Projects() {
 
       <div className="card">
         <h3>AI-OPS-Intelligent-Load-Balancer-Agent</h3>
-        <img src="../../public/Screenshot 2026-01-28 123934.png" alt="AI-OPS Illustration" style={{ width: '100%', height: 'auto' }} />
+        <img src="/AI_OPS.png" alt="AI-OPS Illustration" style={{ width: '100%', height: 'auto' }} />
         <p>
             Built a full-stack AI-driven load-balancing system that uses a fine-tuned LLM to make real-time, explainable routing decisions based on server health metrics instead of static rules.
 Designed a cloud-native architecture with React dashboards, Node.js APIs, workflow orchestration, and deterministic AI validation for production-safe automation.
@@ -16,7 +16,7 @@ Designed a cloud-native architecture with React dashboards, Node.js APIs, workfl
 
       <div className="card">
         <h3>FlowFund : Peer-to-peer lending using Blockchain</h3>
-        <img src="../../public/BlockChain Architecture.png" alt="AI-OPS Illustration" style={{ width: '100%', height: 'auto' }} />
+        <img src="/BlockChain_Architecture.png" alt="AI-OPS Illustration" style={{ width: '100%', height: 'auto' }} />
         <p>Developed a full-stack blockchain-based P2P lending platform enabling secure borrowing, lending, collateral management, and repayments without intermediaries.
 Implemented smart contracts, REST APIs, authentication, and cloud deployment with an AI-assisted risk-scoring approach for fraud detection and credit assessment.</p>
         <p><b>Tech:</b>React, Vite, Node.js, Express.js, MongoDB, Solidity, Ethereum (Hardhat), Ethers.js, Firebase Authentication, Docker, Docker Compose, DigitalOcean, REST APIs, AI-based risk scoring</p>
@@ -25,7 +25,7 @@ Implemented smart contracts, REST APIs, authentication, and cloud deployment wit
 
       <div className="card">
         <h3>Decentralized AI Marketplace : Blockchain-powered platform for AI models and datasets</h3>
-        <img src="../../public/QuantumTrade.png" alt="AI-OPS Illustration" style={{ width: '100%', height: 'auto' }} />
+        <img src="/QuantumTrade.png" alt="AI-OPS Illustration" style={{ width: '100%', height: 'auto' }} />
         <p>Built a decentralized marketplace for AI models and datasets with blockchain-based ownership, revenue management, and wallet-based authentication.
 Implemented a serverless, cloud-native architecture on AWS using Django REST APIs, Infrastructure as Code, CI/CD pipelines, and automated testing for scalability and security.</p>
         <p><b>Tech:</b>Python, Django, Django REST Framework, Solidity, Ethereum, MetaMask, IPFS, AWS (Lambda, S3, DynamoDB, CloudFront, IAM), Terraform, Docker, GitHub Actions, JWT, Google OAuth2</p>
